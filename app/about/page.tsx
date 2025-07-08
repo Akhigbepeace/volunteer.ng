e551391bb886e2ca6855f6e34537e7686245baa2
Akhigbepeace <peaceakhigbe18@gmail.com>
2025-07-08 13:15:18 +0100

    fix: excapas quote

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,9 @@ const About = () => {
       <div className="bg-tertiary min-h-screen text-secondary">
         <div
           className="relative h-[400px] bg-cover bg-center flex items-center justify-center"
-          style={{ backgroundImage: "url('/assets/about-hero.webp')" }}
+          style={{
+            backgroundImage: "url('/assets/about-hero.webp')",
+          }}
         >
           <div className="absolute inset-0 bg-black opacity-50"></div>
 
@@ -21,17 +23,18 @@ const About = () => {
         <div className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8 space-y-8">
           <p className="text-lg leading-relaxed">
             We believe that deep down, every African wants to make a difference—
-            whether it's teaching a child to read, helping a small business
+            whether it&apos;s teaching a child to read, helping a small business
             grow, or cleaning up a neighborhood. But too often, that desire to
             serve gets lost in the shuffle of daily life or the challenge of
             finding the right opportunity.
           </p>
           <p className="text-lg leading-relaxed">
-            That's where Volunteer.ng comes in. We're more than just a platform;
-            we're a community of Africans who've decided that waiting for change
-            isn't enough. We understand the frustration of wanting to help but
-            not knowing where to start, and the struggle organizations face when
-            they need skilled hands but can't find them.
+            That&apos;s where Volunteer.ng comes in. We&apos;re more than just a
+            platform; we&apos;re a community of Africans who&apos;ve decided
+            that waiting for change isn&apos;t enough. We understand the
+            frustration of wanting to help but not knowing where to start, and
+            the struggle organizations face when they need skilled hands but
+            can&apos;t find them.
           </p>
           <p className="text-lg leading-relaxed">
             So we created a space where your Tuesday evening free time can
